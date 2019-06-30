76c15a37d7bc4a33724f297fb3952bd5d1ea2dd9
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2019-06-30 19:23:22 +0100

    Add files via upload

--- help.js
+++ help.js
@@ -11,5 +11,13 @@ module.exports = {
     pair : {
         name: `- **${config.prefix}pair [__item name__ or __item abbreviation__]**`,
         value: `get all available combinations for a specific mini item. __to get all available item names and abbreviations use **${config.prefix}items**__`
-    }
+    },
+    final : {
+        name: `- ${config.prefix}final`,
+        value: `gives you all the available finished items and their abbreviations. __finished item names and abbreviations can be used to get item needed to make that item using **${config.prefix}how** command__`
+    },
+    how : {
+        name: `- ${config.prefix}how [__finished item name__ or __finished item abbreviation__]`,
+        value: `gives you the ingredients needed to make a finished item. __finished item names and abbreviations can be retreived using **${config.prefix}final** command__`
+    },
 }
