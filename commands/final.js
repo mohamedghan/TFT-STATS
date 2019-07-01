fcf29b47996adf48f839a1b4fd7c819d596afb94
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2019-07-01 04:36:39 +0100

    Update final.js

--- commands/final.js
+++ commands/final.js
@@ -1,30 +1,51 @@
-const Discord = require('discord.js');
-const fItems = require('../finalItems');
+const Discord = require("discord.js");
+const fItems = require("../finalItems");
+const { items } = require("../TFT");
 
 module.exports.run = async (bot, msg, args, config) => {
-	const fields = fItems.map(item => ({
-		name: item.name,
-		value: item.abrv
-	}))
+  const fields = fItems.map(item => {
+    let name = item.name;
+    const i = items.filter(it => it.name.toUpperCase() === item.name.toUpperCase())[0];
+    if (i.effects.length !== 0) {
+      const fDesc = i.desc
+        .replace(/%/g, "")
+        .split("@")
+        .map(e =>
+          i.effects.map(eff => eff.name).includes(e)
+            ? i.effects.filter(eff => eff.name == e)[0].value
+            : e
+        )
+        .join("");
+      name += ` (__${fDesc}__)`;
+    } else {
+      name += ` (__${i.desc}__)`;
+    }
 
-	const embed = {
-        title: "ITEMS",
-        description: "these are all the available TFT finished items with their abbreviations:",
-        color: 4886754,
-        footer: {
-          icon_url: bot.user.avatarURL,
-          text: "by @MedDaBeast#2490"
-        },
-        author: {
-          "name": "TFT STATS",
-          "icon_url": msg.author.avatarURL
-        },
-        fields
-	  };
-	  
-	  msg.channel.send('these are all the available items !', { embed })
-}
+    return {
+      name,
+      value: item.abrv
+    };
+  });
+
+  const embed = {
+    title: "ITEMS",
+    description:
+      "these are all the available TFT finished items with their abbreviations:",
+    color: 4886754,
+    footer: {
+      icon_url: bot.user.avatarURL,
+      text: "by @MedDaBeast#2490"
+    },
+    author: {
+      name: "TFT STATS",
+      icon_url: msg.author.avatarURL
+    },
+    fields
+  };
+
+  msg.channel.send("these are all the available items !", { embed });
+};
 
 module.exports.help = {
-	name: "final"
-}
+  name: "final"
+};
