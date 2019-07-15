43ecef16dde6530abbd8bca6c3b6315aef976680
mohamedghan <37912741+mohamedghan@users.noreply.github.com>
2019-07-15 09:58:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,66 @@
-const fs = require('fs');
-const Discord = require('discord.js');
+const fs = require("fs");
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-
-
-const config = require('./config');
+const DBL = require("dblapi.js");
+const dbl = new DBL(process.env.TOKEN);
+const config = require("./config");
 bot.commands = new Discord.Collection();
-fs.readdir('./commands', (err, files) => {
-	if (err) console.log(err);
+fs.readdir("./commands", (err, files) => {
+  if (err) console.log(err);
 
-	let jsfiles = files.filter(f => f.split('.').pop() === 'js');
-	if (jsfiles.length <= 0) {
-		console.log('no commands');
-		return;
-	}
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+  if (jsfiles.length <= 0) {
+    console.log("no commands");
+    return;
+  }
 
-	jsfiles.forEach((f, i) => {
-		let props = require(`./commands/${f}`);
-		console.log(`${f} loaded`);
-		bot.commands.set(props.help.name, props);
-	})
-})
-
-bot.on('ready', () => {
-	bot.user.setActivity(`Team fight Tactics | ${bot.guilds.size  + 1225} servers`,'WATCHING')
-})
-
-
-bot.on('guildCreate', () => {
-	bot.user.setActivity(`Team fight Tactics | ${bot.guilds.size  + 1225} servers`,'WATCHING')
-})
-
-
-bot.on('message', async (msg) => {
-	if (msg.author.bot) return;
-
-	let prefix = config.prefix;
-	let msgarray = msg.content.split(' ');
-	let cmd = msgarray[0];
-	let args = msgarray.slice(1);
-	
-	if(!msg.content.startsWith(prefix)) return;
-
-	let cmdfile = bot.commands.get(cmd.slice(prefix.length));
-
-	if (cmdfile) cmdfile.run(bot, msg, args, config);
+  jsfiles.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
-bot.on('guildMemberAdd', async (mem) => {
-	const dm = await mem.createDM();
-	dm.send(`welcome to the server, if you\'re interested in using me, use the **${config.prefix}help** command to get started!`)
-})
+bot.on("ready", () => {
+  bot.user.setActivity(
+    `Team fight Tactics | ${bot.guilds.size + 1225} servers`,
+    "WATCHING"
+  );
+  dbl.postStats(bot.guilds.size + 1225).then((value) => {
+    console.log(value);
+  });
+});
+
+bot.on("guildCreate", () => {
+  bot.user.setActivity(
+    `Team fight Tactics | ${bot.guilds.size + 1225} servers`,
+    "WATCHING"
+  );
+  dbl.postStats(bot.guilds.size + 1225).then((value) => {
+    console.log(value);
+  });
+});
 
 
-bot.login(process.env.TFT_TOKEN)
+bot.on("message", async msg => {
+  if (msg.author.bot) return;
+
+  let prefix = config.prefix;
+  let msgarray = msg.content.split(" ");
+  let cmd = msgarray[0];
+  let args = msgarray.slice(1);
+
+  let cmdfile = bot.commands.get(cmd.slice(prefix.length));
+
+  if (cmdfile) cmdfile.run(bot, msg, args, config);
+});
+
+bot.on("guildMemberAdd", async mem => {
+  if mem.guild.has('264445053596991498') return;
+  const dm = await mem.createDM();
+  dm.send(
+    "welcome to the server, if you're interested in using me, use the **${config.prefix}help** command to get started!"
+  );
+});
+
+bot.login(process.env.TFT_TOKEN);
